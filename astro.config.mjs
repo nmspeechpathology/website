0b260f8909847050e3ff6f83a28e37429e2a8988
Gabriel Feuilherade <gabrielf3u@gmail.com.au>
2025-07-23 19:36:49 +1000

    Modified some additional configuration settings for porkbun static hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  base: '/', // or '/repo-name/' if using GitHub Pages directly
+});
